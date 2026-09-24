3d12367f186a442670fd2eb0f0e401159963241b
Arvindgupta0111 <akg728988@gmail.com>
2026-09-24 15:29:05 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import "./App.css";
 
-const API_URL = "https://task-management-app-3y24.onrender.com";
+const API_URL = "https://task-management-app-3yz4.onrender.com";
 
 function App() {
   const [isRegister, setIsRegister] = useState(false);
